78fea8dc6ab7525cb5b32f1ac032ab42b50396b3
mallesh1 <86902726+mallesh1@users.noreply.github.com>
2021-08-07 00:36:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -101,24 +101,13 @@ resource "aws_s3_bucket" "terraform_statema" {
     enabled = true
   }
 }
- resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-up-and-running-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
+ 
 terraform {
   backend "s3" {
     # Replace this with your bucket name!
     bucket         = "terraform-up-and-running-state1ma"
     key            = "global/s3/terraform.tfstate"
     region         = "us-east-1"
-    # Replace this with your DynamoDB table name!
-    dynamodb_table = "terraform-up-and-running-locks"
-    encrypt        = true
   }
 }
  
