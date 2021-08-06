97962a20771fb7b906f7a2481f9b63f91058e231
mallesh1 <86902726+mallesh1@users.noreply.github.com>
2021-08-07 00:02:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -101,5 +101,13 @@ resource "aws_s3_bucket" "terraform_state" {
     enabled = true
   }
 }
- 
+ resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-up-and-running-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
  
